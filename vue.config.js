6b1ab6164467c5261cc323914344daedf5214eff
wuyou <wuyou@mikeo2o.com>
2020-09-15 18:16:55 +0800

    更新demo

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,38 @@ const plugins =
 
 module.exports = {
   pages: pagesObj,
+  // // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+
   configureWebpack: {
+    entry: {
+      content: "./src/content/index.js"
+    },
+    output: {
+      filename: "js/[name].js"
+    },
     plugins: [CopyWebpackPlugin(plugins)]
+  },
+  css: {
+    extract: {
+      filename: "css/[name].css"
+      // chunkFilename: 'css/[name].css'
+    }
+  },
+  chainWebpack: config => {
+    // 处理字体文件名，去除hash值
+    const fontsRule = config.module.rule("fonts");
+
+    // 清除已有的所有 loader。
+    // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后。
+    fontsRule.uses.clear();
+    fontsRule
+      .test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/i)
+      .use("url")
+      .loader("url-loader")
+      .options({
+        limit: 1000,
+        name: "fonts/[name].[ext]"
+      });
   }
 };
