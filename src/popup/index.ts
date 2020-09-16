32e4bc7e79091293feb0394609c03693b96253a6
wuyou <wuyou@mikeo2o.com>
2020-09-16 14:09:19 +0800

    修改bug 文件类型错误

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import AppComponent from "./App/App.vue";
-import {
-  Card
-} from "element-ui";
+import { Button, Card } from "element-ui";
 
 Vue.component("app-component", AppComponent);
 
+Vue.use(Button);
 Vue.use(Card);
 
 new Vue({
